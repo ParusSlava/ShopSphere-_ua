5bd1cf1f9e1ceb0690fe84b858147c0faa181dd3
Iaroslava Parusenkova <y.parusenkova@gmail.com>
2026-07-14 14:25:03 +0200

    Update queries.sql
    1.2 SQL

--- queries.sql
+++ queries.sql
@@ -14,3 +14,31 @@ GROUP BY
 ORDER BY
     o.order_year,
     total_net_revenue DESC;
+
+
+-- 1.2. Знайдіть топ-10 клієнтів за загальною сумою витрат. Виведіть їхній регіон, канал залучення і скільки замовлень вони зробили.
+SELECT
+    c.customer_id,
+    c.region,
+    c.acquisition_chan,
+
+    ROUND(SUM(o.net_amount), 2) AS total_spend,
+
+    COUNT(o.order_id) AS order_count,
+
+    ROUND(AVG(o.net_amount), 2) AS average_order_value
+
+FROM customers AS c
+
+INNER JOIN orders AS o
+    ON c.customer_id = o.customer_id
+
+GROUP BY
+    c.customer_id,
+    c.region,
+    c.acquisition_chan
+
+ORDER BY
+    total_spend DESC
+
+LIMIT 10;
